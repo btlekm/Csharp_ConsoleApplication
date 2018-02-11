8a50d1368033f9093d8273a2b60b1a9f021fbd53
btlekm <36238999+btlekm@users.noreply.github.com>
2018-02-11 22:55:51 +0300

    Update Program.cs

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,15 +10,16 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             //Klavyeden girilen 3 sayının ortalamasını hesaplayan programı C#.NET ile kodlayın.!
-            string say1,say2,say3;
+            int say1,say2,say3,bolum;
             const int bol3 = 3;
-            Console.Write("1.Sayıyı Girin: ");
-            say1 = Console.ReadLine();
+            Console.WriteLine("1.Sayıyı Girin: ");
+            say1 = int.Parse(Console.ReadLine());
             Console.Write("2.Sayıyı Girin: ");
-            say2 = Console.ReadLine();
+            say2 = int.Parse(Console.ReadLine());
             Console.Write("3.Sayıyı Girin: ");
-            say3 = Console.ReadLine();
-            Console.WriteLine("{0} + {1} + {2} / {3} = {4}",say1,say2,say3,bol3, ((Convert.ToInt16(say1) + Convert.ToInt16(say2) + Convert.ToInt16(say3)) / (bol3)));
+            say3 = int.Parse(Console.ReadLine());
+            bolum=(say1+say2+say3)/bol3;
+            Console.WriteLine("{0} + {1} + {2} / {3} = {4}",say1,say2,say3,bol3, bolum );
 
             Console.ReadKey();
         }
